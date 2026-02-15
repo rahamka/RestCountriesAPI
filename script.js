157cb28cbdb89d7098e62748c8f24c4c08c75181
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-02-15 11:48:59 +0500

    added the filter function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const countryContainer = document.querySelector(".countries-container");
+const filterByRegion = document.getElementById("filter-by-region");
+
 let countryCard;
 fetch(
   "https://restcountries.com/v3.1/all?fields=name,capital,tld,currencies,languages,borders,subregion,population,region,flags",
@@ -25,3 +27,32 @@ fetch(
       }
     });
   });
+
+filterByRegion.addEventListener("change", (evt) => {
+  // Note: we're creating new cards after cleaning the previous cards of the countryContainer cards.
+  fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+    .then((data) => data.json())
+    .then((countries) => {
+      countryContainer.innerHTML = "";
+      console.log("hello");
+      countries.forEach((country) => {
+        countryCard = document.createElement("a");
+        countryCard.classList.add("country-card");
+        countryCard.href = `/RestCountriesAPI/countryFolder/country.html?name=${country.name.common}`;
+        countryCard.innerHTML = `
+              <img src="${country.flags.svg}" alt="${country.name.common}" />
+              <div class="card-text">
+                  <h3 class="card-title">${country.name.common}</h3>
+                  <p><b>Population: </b>${country.population.toLocaleString("en-IN")}</p>
+                  <p><b>Region: </b>${country.region}</p>
+                  <p><b>Capital: </b>${country.capital}</p>
+              </div>
+      `;
+        try {
+          countryContainer.append(countryCard);
+        } catch (err) {
+          // console.log(err);
+        }
+      });
+    });
+});
